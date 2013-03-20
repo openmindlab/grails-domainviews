fe607b01f38c8d0c1f7c58cb389ba602a0f9bc34
Carlo Colombo <carlo.colombo@openmindonline.it>
2013-03-20 11:54:47 +0100

    fix

--- grails-app/domain/it/openmindonline/domainviews/test/dataobjects/DomainWithEnumeration.groovy
+++ grails-app/domain/it/openmindonline/domainviews/test/dataobjects/DomainWithEnumeration.groovy
@@ -2,9 +2,8 @@ package it.openmindonline.domainviews.test.dataobjects
 
 class DomainWithEnumeration {
   Enumeration property = Enumeration.ONE
-  
-  enum Enumeration {
-    ONE,TWO
-  }
 }
 
+enum Enumeration {
+  ONE,TWO
+}
